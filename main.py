74cdd1df5c989c1f88cfc6d5d55bf9d7837c848f
DESKTOP-5A4MG6P\RafalLietzau-3TP <kremufka69420@gmail.com>
2024-10-07 12:06:29 +0200

    Dodano dokumentacje do aplikacji

---
+++ main.py
@@ -0,0 +1,14 @@
+# ***********************************************
+# Nazwa klasy: Film
+# Pola:
+#   _tytul - przechowuje tytuł filmu, ograniczony do 20 znaków
+#   _liczba_wypozyczen - przechowuje liczbę wypożyczeń filmu
+# Metody:
+#   ustaw_tytul(tytul) - ustawia tytul filmu (maks. 20 znaków)
+#   pobierz_tytul() - zwraca tytuł filmu
+#   pobierz_liczba_wypozyczen() - zwraca liczbę wypożyczeń
+#   zwieksz_liczbe_wypozyczen() - zwiększa liczbę wypożyczeń
+#   wyswietl_informacje() - wyświetla tytuł oraz liczbę wypożyczalni, przechowując tytuł oraz liczbe wypożyczeń
+# Informacje: klasa film reprezentuje film w systemie wypożyczalni, przechowując tytuł oraz liczbę wypożyczeń
+# autor: Rafał Lietzau      
+# ***********************************************
