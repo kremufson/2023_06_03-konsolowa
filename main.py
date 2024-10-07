e829b3850056fde98e862f73c72d98cda7e219f2
DESKTOP-5A4MG6P\RafalLietzau-3TP <kremufka69420@gmail.com>
2024-10-07 12:20:21 +0200

    Dodano główną logike aplikacji

--- main.py
+++ main.py
@@ -12,3 +12,33 @@
 # Informacje: klasa film reprezentuje film w systemie wypożyczalni, przechowując tytuł oraz liczbę wypożyczeń
 # autor: Rafał Lietzau      
 # ***********************************************
+class film:
+    def wyswietl_informacje(self):
+
+
+if __name__ == "__main__":
+
+    #Inicjalizacja obiektu film z domyślnymi wartościami
+    film = film()
+
+    #Wyświetalnie początkowych informacji o filmie
+    print("Początkowe dane o filmu:")
+    film.wyswietl_informacje()
+
+    #Testowanie metody ustaw_tytul, ustawiając nowy tytuł
+    print("\nUstawiamy nowy tytuł filmu 'Incepcja'")
+    film.ustaw_tytul("Incepcja")
+    print("\nPo ustawieniu tytułu:")
+    film.wyswietl_informacje=()
+
+    #Testowanie metody pobierz_tytul, pobierając tytuł filmu
+    print("\nAktualny tytuł filmu:", film.pobierz_tytul())
+
+    #Testowanie inkrementacji liczby wypożyczeń
+    print("\nZwiększamy liczbę wypożyczeń dwukrotnie...")
+    film.zwieksz_liczbe_wypozyczen()
+    film.zwieksz_liczbe_wypozyczen()
+    film.wyswietl_informacje()
+
+    #Wyświetlanie finalnej liczby wypożyczeń
+    print("\nOstateczna liczba wypożyczeń", film.pobierz_liczbe_wypozyczen())
